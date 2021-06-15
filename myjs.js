c8f3fd261b96b8c19ba2cda4bf5033a5b8b19cad
zm20537 <zm20537@josai.ac.jp>
2021-06-15 01:46:08 +0000

    06.15

--- myjs.js
+++ myjs.js
@@ -2,5 +2,6 @@
 // alert(1+1);
 // alert(Math.sqrt(1+1));
 // alert(Math.sqrt(2)+Math.sqrt(3));
-//alert(1/0); /*Infinitiと表示された*/
-alert(0/0);
+//alert(1/0); /*Infinitiと表示された(JavaScriptが扱える限界を越えたの意)*/
+//alert(0/0); /*NaNと表示された(Not A Numberの略。計算結果が数値として表せないの意)*/
+alert(2+3);
