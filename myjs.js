7d0f56ab484425f0dc00f38dcbde499a81db2443
zm20537 <zm20537@josai.ac.jp>
2021-06-15 01:49:22 +0000

    06.15

--- myjs.js
+++ myjs.js
@@ -5,4 +5,5 @@
 // alert(1/0); /*Infinitiと表示された(JavaScriptが扱える限界を越えたの意)*/
 // alert(0/0); /*NaNと表示された(Not A Numberの略。計算結果が数値として表せないの意)*/
 // alert(2+3); /*5*/
-alert('2'+'3');
+// alert('2'+'3'); /*23*/
+alert(Number('2')+Number('3'));
