1e8ca3ece2763c20f7384f1b7e68ce0cfffda505
zm20537 <zm20537@josai.ac.jp>
2021-06-29 01:43:31 +0000

    06.29

--- ex6-5.js
+++ ex6-5.js
@@ -1,5 +1,5 @@
 //2.2変数への代入
-var who65 = 'world';
+/*var who65 = 'world';
 function sayhello65(){
     alert('Hello,' + who65 + '!');
 }
@@ -8,7 +8,18 @@ function taro65(){
 }
 function hanako65(){
     who65 = '花子';
-}
+}*/
 /*説明：who65がはじめは"world"が代入されているため、表示される文字は、"Hello"と"world"となる。
 しかし、太郎や花子のボタンを押すことにより、who65に太郎（花子）を代入される。
 そのため、もう一度あいさつボタンを押すと"Hello, 太郎(花子)"となった。*/
+
+var who65 = '誰かが';
+function sayhello65(){
+    alert(who65 + 'が来た');
+}
+function taro65(){
+    who65 = '太郎';
+}
+function hanako65(){
+    who65 = '花子';
+}
