96d7bc86cd556ef56a3989acb435821e773edb1f
zm20537 <zm20537@josai.ac.jp>
2021-07-13 01:40:20 +0000

    07.13

--- myjs.js
+++ myjs.js
@@ -1,5 +1,5 @@
-// alert('Hello,Osamu!');
-// alert(1+1);
+// alert('Hello,Tetta!');
+alert(1+1);
 // alert(Math.sqrt(1+1));
 // alert(Math.sqrt(2)+Math.sqrt(3));
 // alert(1/0); /*Infinitiと表示された(JavaScriptが扱える限界を越えたの意)*/
